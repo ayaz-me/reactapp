5582b974399f7753f082f4d222cc01794ae3aee0
Mehmet Ayaz <120301068@student.beykent.edu.tr>
2022-01-26 00:46:48 +0300

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import User from './components/User';
+
 
 function App() {
-  const pi = 3.14;
-  const isAuth = false;
   return (
-    <div className="App">
-      <h1>{10 % 3}</h1>
-      <h1>{"Mehmet".toUpperCase()}</h1>
-      <h1>{pi * 50}</h1>
-      <div>
-        {isAuth ? <p> Kullanıcı Kayıtlı</p> : <p>null</p>}
-      </div>
-      <User />
+    <div className="container">
+      <h1>App Component</h1>
     </div>
   );
 }

fa87c4cc416694b4471d158127e2060ed2bef8ec
Mehmet Ayaz <120301068@student.beykent.edu.tr>
2022-01-27 23:38:26 +0300

    PropTypes & Default Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import User from './components/User';
 
-function App() {
-  return (
-    <div className="container">
-      <Navbar title="User App" />
-      <hr/>
-      <User 
-        name="Mehmet" 
-        job="Software Engineer" 
-        team="Besiktas"
-      />
-      <hr/>
-      <User 
-        name="Onur" 
-        job="Software Engineer" 
-        team="Fenerbahce"
-      />
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className="container">
+        <Navbar />
+        <hr/>
+        <User 
+          name="Mehmet" 
+          job="Software Engineer" 
+          team="Besiktas"
+        />
+        <hr/>
+        <User 
+          name="Onur" 
+          job="Software Engineer" 
+          team="Fenerbahce"
+        />
+        <User/>
+      </div>
+    );
+  }
 }
 
 export default App;
